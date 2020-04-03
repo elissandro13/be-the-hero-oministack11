c1e20a9341ac3b85342034e79bad74f0c8e1fb3c
elissandro13 <ecjunior19@gmail.com>
2020-04-02 22:55:39 -0300

    Fixing

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,13 +1,8 @@
 const knex = require('knex');
 const configuration = require('../../knexfile');
-<<<<<<< HEAD
 const config = process.env.NODE_ENV === 'test' ? configuration.test : configuration.development;
 
 const  connection = knex(config);
 
-=======
-
-const  connection = knex(configuration.development);
->>>>>>> ac37d29a924af3d2616237c67d4ac0e2d5d22a4b
 
 module.exports = connection;
